807940b62b997168c2406ef85a06647a001b549c
Marcus Amaral Rolim <m4rkux@gmail.com>
2026-03-05 20:13:00 -0300

    Update adapter

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({ runtime: "nodejs22.x" }),
+    adapter: adapter(),
     alias: {
       $lib: "./src/lib",
     },
